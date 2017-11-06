f91ccada68b7ba07688db5816bd2183116906e0f
eoneoff <eoneoff@gmail.com>
2017-11-06 18:36:26 +0200

    First commit of BooksGridView. Entity model and categories dropdown list

---
+++ BooksGridView/BooksGridView/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BooksGridView.Global" Language="C#" %>
---
+++ BooksGridView/BooksGridView/WebForm1.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BooksGridView
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        booksEntities db;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            db = new booksEntities();
+            var cathegories = (from c in db.Spr_kategory select c).ToList();
+            Categories.DataSource = cathegories;
+            if(!IsPostBack)
+            {
+                Categories.DataTextField = "Category";
+                Categories.DataValueField = "Id";
+                Categories.DataBind();
+            }
+        }
+    }
+}
