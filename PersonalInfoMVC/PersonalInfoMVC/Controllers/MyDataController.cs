0ea893378cf460e45422e6b533d4a7ae0c4f4ce1
eoneoff <eoneoff@gmail.com>
2017-12-04 00:41:32 +0200

    PersonalInfoMVCPageOne

---
+++ PersonalInfoMVC/PersonalInfoMVC/Controllers/MyDataController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonalInfoMVC.Controllers
+{
+    public class MyDataController : Controller
+    {
+        string[] cities = { "Киев", "Чернигов", "Харьков", "Львов", "Полтава", "Сумы", "Одесса" };
+        string[] hobbies = { "Туризм", "Музыка", "Футбол", "Рисование", "Кино", "Танцы" };
+
+        // GET: MyData
+        public ActionResult Index()
+        {
+            ViewBag.Cities = new SelectList(cities);
+            ViewBag.Hobbies = new MultiSelectList(hobbies);
+            return View();
+        }
+    }
+}
---
+++ PersonalInfoMVC/PersonalInfoMVC/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PersonalInfoMVC.Models
+{
+    public class Person
+    {
+        public string FamilyName;
+        public string FirstName;
+        public string Patronim;
+        public string Sex;
+        public string City;
+        public DateTime? DateOfBirth;
+        public string[] Hobbies;
+    }
+}
